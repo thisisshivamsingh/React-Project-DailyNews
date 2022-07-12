83bd5151a8d3ed0b1c9de037f9da0585c877962a
Shivam <thisisshivam06@gmail.com>
2022-07-13 02:19:08 +0530

    Post by ID

--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -1,5 +1,49 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+// import Moment from "react-moment";
+
+import { Spinner } from "react-bootstrap";
+import { fetchPostById } from "../../store/utils/thunks";
+
 const PostComponent = () => {
-  return <>PostComponent</>;
+  const posts = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+  let params = useParams();
+
+  useEffect(() => {
+    dispatch(fetchPostById(params.id));
+  }, []);
+
+  return (
+    <>
+      {posts.postById ? (
+        <div className="article_container">
+          <h1>{posts.postById.title}</h1>
+          <div
+            style={{ background: `url(${posts.postById.imagexl})` }}
+            className="image"
+          ></div>
+          <div className="author">
+            Created by: <span>{posts.postById.author} - </span>
+            {/* <Moment format="DD MMMM">{posts.postById.createdAt}</Moment> */}
+            <div className="mt-3 content">
+              <div
+                dangerouslySetInnerHTML={{ __html: posts.postById.content }}
+              ></div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+      {posts.loading ? (
+        <div style={{ textAlign: "center" }}>
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      ) : null}
+    </>
+  );
 };
 
 export default PostComponent;
--- src/store/utils/thunks.js
+++ src/store/utils/thunks.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-const URL_SERVE = "http://localhost:3001";
+const URL_SERV = "http://localhost:3001";
 
 export const fetchPosts = createAsyncThunk(
   "posts/fetchPosts",
@@ -8,7 +8,7 @@ export const fetchPosts = createAsyncThunk(
     try {
       // http://localhost:3004/posts?_page=1&_limit=6&_order=desc&_sort=id
       const response = await axios.get(
-        `${URL_SERVE}/posts?_page=${page}&_limit=${limit}&_order=${order}&_sort=id`
+        `${URL_SERV}/posts?_page=${page}&_limit=${limit}&_order=${order}&_sort=id`
       );
 
       const prevState = getState().posts;
@@ -18,6 +18,17 @@ export const fetchPosts = createAsyncThunk(
         page: page,
         end: response.data.length === 0 ? true : false,
       };
+    } catch (error) {
+      throw error;
+    }
+  }
+);
+export const fetchPostById = createAsyncThunk(
+  "posts/fetchPostById",
+  async (id) => {
+    try {
+      const response = await axios.get(`${URL_SERV}/posts/${id}`);
+      return response.data;
     } catch (error) {
       throw error;
     }
